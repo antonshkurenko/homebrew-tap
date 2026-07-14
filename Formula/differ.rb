de7a8649f8a0e5096d37157f1d8ddc86b816d191
Anton Shkurenko <anton@twingate.com>
2026-07-14 09:52:49 +0200

    repoint at v0.1.0 (0.1.1/0.1.2 withdrawn)

--- Formula/differ.rb
+++ Formula/differ.rb
@@ -1,15 +1,15 @@
 class Differ < Formula
   desc "Track what you've already reviewed in diffs"
   homepage "https://github.com/antonshkurenko/differ"
-  version "0.1.2"
+  version "0.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/antonshkurenko/differ/releases/download/v0.1.2/differ-macos-arm64"
-      sha256 "6cacda351b1266ca6584a04a058d4cce673f4b5893f39051bfd31da8656b7a02"
+      url "https://github.com/antonshkurenko/differ/releases/download/v0.1.0/differ-macos-arm64"
+      sha256 "5a8dd5bb53dd4126f3a29da4a9cdeeb39bfffcc73065c1388a27d45c59f7054b"
     else
-      url "https://github.com/antonshkurenko/differ/releases/download/v0.1.2/differ-macos-x64"
-      sha256 "a39b64533a5540c8bd343b52b51394f57a0fa8c0c4f256f3eef3d7eb0dd7610e"
+      url "https://github.com/antonshkurenko/differ/releases/download/v0.1.0/differ-macos-x64"
+      sha256 "f79fe34ea88e5d66d548a3c465acbc45f03c96c4805a0fcabbc008501463917c"
     end
   end
 
